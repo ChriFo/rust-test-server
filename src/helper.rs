4153db247ff1a52a0d75195e2356b95a5b94c8cb
Christian Fochler <mail@christianfochler.de>
2018-08-08 20:47:03 +0200

    add helper mod - read file func - rand string func

---
+++ src/helper.rs
@@ -0,0 +1,16 @@
+use rand::{self, distributions::Alphanumeric, Rng};
+use std::{fs::File, io::Read};
+
+pub fn create_rand_string(size: usize) -> String {
+    rand::thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(size)
+        .collect::<String>()
+}
+
+pub fn read_file(file: &str) -> String {
+    let mut file = File::open(file).unwrap();
+    let mut content = String::new();
+    let _ = file.read_to_string(&mut content);
+    content
+}
