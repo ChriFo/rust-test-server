059621654b401244ebaceb0ad0d1cc5a757f3f9c
Christian Fochler <mail@christianfochler.de>
2018-08-08 20:47:24 +0200

    adjust tests to use new helper

--- tests/server.rs
+++ tests/server.rs
@@ -1,11 +1,8 @@
-extern crate rand;
 extern crate reqwest;
 extern crate test_server;
 
-use rand::{distributions::Alphanumeric, Rng};
 use reqwest::StatusCode;
-use std::{fs::File, io::Read};
-use test_server::{HttpResponse, Request, TestServer};
+use test_server::{helper, HttpResponse, Request, TestServer};
 
 #[test]
 fn start_server_at_given_port() {
@@ -37,7 +34,7 @@ fn restart_server_at_same_port() {
 fn validate_client_request() {
     let server = TestServer::new(0, |_| HttpResponse::Ok().into());
 
-    let request_content = create_rand_string(100);
+    let request_content = helper::create_rand_string(100);
     let client = reqwest::Client::new();
     let _ = client
         .post(&server.url())
@@ -64,12 +61,15 @@ fn validate_client_request() {
 #[test]
 fn not_necessary_to_fetch_request_from_server() {
     let server = TestServer::new(0, |_| {
-        let content = read_file("tests/sample.json");
+        let content = helper::read_file("tests/sample.json");
         HttpResponse::Ok().body(content).into()
     });
     let mut response = reqwest::get(&server.url()).unwrap();
 
-    assert_eq!(read_file("tests/sample.json"), response.text().unwrap());
+    assert_eq!(
+        helper::read_file("tests/sample.json"),
+        response.text().unwrap()
+    );
 }
 
 #[test]
@@ -83,18 +83,4 @@ fn fetch_2nd_request_from_server() {
 
     assert_eq!(requests.len(), 2);
     assert_eq!("2", requests[1].body);
-}
-
-fn create_rand_string(size: usize) -> String {
-    rand::thread_rng()
-        .sample_iter(&Alphanumeric)
-        .take(size)
-        .collect::<String>()
-}
-
-fn read_file(file: &str) -> String {
-    let mut file = File::open(file).unwrap();
-    let mut content = String::new();
-    let _ = file.read_to_string(&mut content);
-    content
 }
