1c8554b9d50fda066593476ec9458f5ac5f3db5a
Christian Fochler <mail@christianfochler.de>
2018-08-11 20:29:25 +0200

    restructure middleware mod

--- src/middlware.rs
+++ src/middlware.rs
@@ -11,18 +11,7 @@ impl<S> Middleware<S> for ShareRequest {
     fn start(&self, req: &HttpRequest<S>) -> Result<Started> {
         let id = self.id;
 
-        let headers = req
-            .headers()
-            .iter()
-            .map(|(k, v)| {
-                (
-                    k.as_str().to_string(),
-                    v.to_str()
-                        .expect("Failed to convert header value")
-                        .to_string(),
-                )
-            }).collect::<HashMap<_, _>>();
-
+        let headers = extract_headers(req);
         let method = req.method().to_string();
         let path = req.path().to_string();
 
@@ -33,23 +22,45 @@ impl<S> Middleware<S> for ShareRequest {
                 body.extend_from_slice(&chunk);
                 Ok(body)
             }).and_then(move |body| {
-                let mut queue = match QUEUE.lock().remove(&id) {
-                    Some(queue) => queue,
-                    None => vec![],
-                };
-
-                queue.push(Request {
-                    body: String::from_utf8(body.to_vec()).expect("Failed to extract request body"),
-                    headers,
-                    method,
-                    path,
-                });
-
-                QUEUE.lock().insert(id, queue);
-
+                queue_request(id, &body, headers, method, path);
                 Ok(None)
             });
 
         Ok(Started::Future(Box::new(fut)))
     }
+}
+
+fn extract_headers<S>(req: &HttpRequest<S>) -> HashMap<String, String> {
+    req.headers()
+        .iter()
+        .map(|(k, v)| {
+            (
+                k.as_str().to_string(),
+                v.to_str()
+                    .expect("Failed to convert header value")
+                    .to_string(),
+            )
+        }).collect::<HashMap<_, _>>()
+}
+
+fn queue_request(
+    id: u8,
+    body: &BytesMut,
+    headers: HashMap<String, String>,
+    method: String,
+    path: String,
+) {
+    let mut queue = match QUEUE.lock().remove(&id) {
+        Some(queue) => queue,
+        None => vec![],
+    };
+
+    queue.push(Request {
+        body: String::from_utf8(body.to_vec()).expect("Failed to extract request body"),
+        headers,
+        method,
+        path,
+    });
+
+    QUEUE.lock().insert(id, queue);
 }
