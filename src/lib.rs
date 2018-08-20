b58a713cfe7ea73a06b1c345e3b6fd352860189d
Christian Fochler <mail@christianfochler.de>
2018-08-20 13:23:01 +0200

    add helper tests

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,10 @@ extern crate bytes;
 extern crate crossbeam_channel as channel;
 extern crate futures;
 extern crate rand;
+#[cfg(test)]
+extern crate spectral;
 
 pub use actix_web::{HttpRequest, HttpResponse};
-pub use rand::random;
 pub use requests::{Request, RequestReceiver};
 pub use server::TestServer;
 
